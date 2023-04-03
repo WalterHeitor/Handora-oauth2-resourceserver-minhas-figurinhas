bfc0654a07f9d127495fcfe8c0ef01315595c3ae
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2023-04-03 17:53:20 -0300

    feat: refactor test3

--- oauth2-resourceserver-minhas-figurinhas-master/src/test/java/br/com/zup/edu/minhasfigurinhas/albuns/listagem/ListaAlbunsControllerTest.java
+++ oauth2-resourceserver-minhas-figurinhas-master/src/test/java/br/com/zup/edu/minhasfigurinhas/albuns/listagem/ListaAlbunsControllerTest.java
@@ -6,10 +6,12 @@ import br.com.zup.edu.minhasfigurinhas.albuns.AlbumRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.List;
 
 import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -27,21 +29,24 @@ class ListaAlbunsControllerTest extends SpringBootIntegrationTest {
     public void deveListarTodosOsAlbuns() throws Exception {
         // cenário
         List.of(
-            new Album("DBZ", "Album do DBZ", "rafael.ponte"),
-            new Album("CDZ", "Album do CDZ", "jordi.silva"),
-            new Album("Naruto", "Album do Naturo", "yuri.matheus")
+                new Album("DBZ", "Album do DBZ", "rafael.ponte"),
+                new Album("CDZ", "Album do CDZ", "jordi.silva"),
+                new Album("Naruto", "Album do Naturo", "yuri.matheus")
         ).forEach(album -> {
             repository.save(album);
         });
 
         // ação e validação
-        mockMvc.perform(GET("/api/albuns"))
+        mockMvc.perform(GET("/api/albuns")
+                        .with(jwt()
+                                .authorities(new SimpleGrantedAuthority("SCOPE_minhas-figurinhas:read"))
+                        ))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(3)))
                 .andExpect(jsonPath("$[0].titulo").value("CDZ"))
                 .andExpect(jsonPath("$[1].titulo").value("DBZ"))
                 .andExpect(jsonPath("$[2].titulo").value("Naruto"))
-                ;
+        ;
     }
 
     @Test
@@ -50,10 +55,48 @@ class ListaAlbunsControllerTest extends SpringBootIntegrationTest {
         repository.deleteAll();
 
         // ação e validação
-        mockMvc.perform(GET("/api/albuns"))
+        mockMvc.perform(GET("/api/albuns")
+                .with(jwt()
+                        .authorities(new SimpleGrantedAuthority("SCOPE_minhas-figurinhas:read"))
+                ))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$").isEmpty())
-                ;
+        ;
+    }
+
+    @Test
+    public void naoDeveListarTodosOsAlbunsSemToken() throws Exception {
+        // cenário
+        List.of(
+                new Album("DBZ", "Album do DBZ", "rafael.ponte"),
+                new Album("CDZ", "Album do CDZ", "jordi.silva"),
+                new Album("Naruto", "Album do Naturo", "yuri.matheus")
+        ).forEach(album -> {
+            repository.save(album);
+        });
+
+        // ação e validação
+        mockMvc.perform(GET("/api/albuns"))
+                .andExpect(status().isUnauthorized())
+        ;
+    }
+
+    @Test
+    public void naoDeveListarTodosOsAlbunsSemEscopo() throws Exception {
+        // cenário
+        List.of(
+                new Album("DBZ", "Album do DBZ", "rafael.ponte"),
+                new Album("CDZ", "Album do CDZ", "jordi.silva"),
+                new Album("Naruto", "Album do Naturo", "yuri.matheus")
+        ).forEach(album -> {
+            repository.save(album);
+        });
+
+        // ação e validação
+        mockMvc.perform(GET("/api/albuns")
+                .with(jwt()))
+                .andExpect(status().isForbidden())
+        ;
     }
 
 }
